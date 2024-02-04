93979433f0655de1553a72625c3cda78423f2428
Laila-dAM <lailamota.238@gmail.com>
2024-02-04 12:34:14 -0300

    Verify login: email @gmail.com, password >=8 characters, with special character. Displays success or invalid alert

--- login.js
+++ login.js
@@ -2,4 +2,11 @@ function login(){
 
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
+
+    if (email.endsWith("@gmail.com") && password.length >= 8 && /[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+        alert("Login Successful");
+    }
+    else {
+        alert ("Incalid email or password");
+    }
 }
